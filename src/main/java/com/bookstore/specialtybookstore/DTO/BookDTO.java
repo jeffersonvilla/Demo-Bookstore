82da49dd3bd48eb9aa5e0e09d492c528c6835df2
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-12-27 18:51:46 -0500

    refactor import

--- src/main/java/com/bookstore/specialtybookstore/DTO/BookDTO.java
+++ src/main/java/com/bookstore/specialtybookstore/DTO/BookDTO.java
@@ -1,7 +1,6 @@
 package com.bookstore.specialtybookstore.DTO;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class BookDTO {
