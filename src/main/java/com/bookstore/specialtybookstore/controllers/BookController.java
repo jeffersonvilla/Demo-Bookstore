e46835102e00b05c305ac7d494bd8c5195d03636
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-12-29 17:15:26 -0500

    cross origin config for BookController

--- src/main/java/com/bookstore/specialtybookstore/controllers/BookController.java
+++ src/main/java/com/bookstore/specialtybookstore/controllers/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api/v1/book")
 @Tag(name = "Book Controller", description = "Operations related to books")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class BookController {
 
     private IBookService bookService;
