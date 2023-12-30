309c565c4a3e0f8f308152b65d4693ec083208bb
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-12-30 13:17:07 -0500

    change messages to other folder

--- src/main/java/com/bookstore/specialtybookstore/exceptions/error_messages/ExceptionMessages.java
+++ src/main/java/com/bookstore/specialtybookstore/exceptions/error_messages/ExceptionMessages.java
@@ -1,4 +1,4 @@
-package com.bookstore.specialtybookstore.exceptions;
+package com.bookstore.specialtybookstore.exceptions.error_messages;
 
 public class ExceptionMessages {
     public static final String ERROR_NULL_BOOK = "The book should not be null";
@@ -7,4 +7,5 @@ public class ExceptionMessages {
     public static final String ERROR_TITLE_LENGTH_EXCEEDS = "The book title length exceeds the maximum allowed (255 characters)";
     public static final String ERROR_KEYWORDS_LENGTH_EXCEEDS = "The keywords length exceeds the maximum allowed (255 characters)";
     public static final String BOOK_CREATION_ERROR = "Error creating the book";
+    public static final String ERROR_GETTING_ALL_BOOKS = "Failed to retrieve all books from the database.";
 }
